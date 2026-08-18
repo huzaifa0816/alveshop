d5b7c5300841b9e31c6c1743ef4de7290b096cab
huzaifa0816 <134155483+huzaifa0816@users.noreply.github.com>
2026-08-17 22:58:36 -0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "lvjcvmpytnwweckrhtjf";
+const SUPABASE_KEY = "sb_publishable_g8SteLwdjRXBY-BFjXr7_g_NbKk_wt-";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 function buy(product){alert(product + " selected!\n\nConnect your payment gateway here to complete checkout.");}
